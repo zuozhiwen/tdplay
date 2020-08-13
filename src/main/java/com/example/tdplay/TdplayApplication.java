b9a48c9e53226a4b844db470de23e116ac0907a4
thunderobot <zzwqqmail@qq.com>
2020-08-13 12:11:37 +0800

    init project

---
+++ src/main/java/com/example/tdplay/TdplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tdplay/TdplayApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.tdplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdplayApplicationTests {
+
+}
